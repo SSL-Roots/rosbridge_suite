2be890dd39673686e8f640a7ca71d65eb6739113
tilt-silvie <tilt.silvie@gmail.com>
2024-03-11 23:21:22 +0900

    Fix length-limited list types
    ref: https://github.com/RobotWebTools/rosbridge_suite/pull/840/commits/cc80d23e83c8e8ae1fd824d56928568ddd8d923f

--- rosbridge_library/src/rosbridge_library/internal/message_conversion.py
+++ rosbridge_library/src/rosbridge_library/internal/message_conversion.py
@@ -90,7 +90,8 @@ ros_primitive_types = [
 ]
 ros_header_types = ["Header", "std_msgs/Header", "roslib/Header"]
 ros_binary_types = ["uint8[]", "char[]", "sequence<uint8>", "sequence<char>"]
-list_tokens = re.compile("<(.+?)>")
+# Remove the list type wrapper, and length specifier, from rostypes i.e. sequence<double, 3>
+list_tokens = re.compile("<(.+?)(, \d+)?>")
 bounded_array_tokens = re.compile(r"(.+)\[.*\]")
 ros_binary_types_list_braces = [
     ("uint8[]", re.compile(r"uint8\[[^\]]*\]")),
